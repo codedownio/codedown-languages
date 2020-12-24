01e33dde1cf357a43eb5e4b329a1609d402e7864
Tom McLaughlin <tom@codedown.io>
2020-12-24 12:39:58 -0700

    More options in package.sh, but probably don't need this after all

--- package.sh
+++ package.sh
@@ -2,4 +2,7 @@
 set -euo pipefail
 
 rm -f nixexprs.tar.bz2
-tar --exclude='.git' -cvjSf nixexprs.tar.bz2 *
+tmp=$(mktemp -d)
+tar --exclude='.git' --transform='s,^\.,base,' -cvjSf $tmp/nixexprs.tar.bz2 .
+tar --exclude='.git' --transform='s,^\.,base,' -cvJSf $tmp/nixexprs.tar.xz .
+echo "Wrote archives to $tmp/"
