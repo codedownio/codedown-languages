04806e5ef03887a2e927c8079168abe3fd2266f5
Tom McLaughlin <tom@codedown.io>
2021-01-25 08:58:03 -0800

    Add sysstat to path

--- tools/powerline/default.nix
+++ tools/powerline/default.nix
@@ -32,13 +32,13 @@ runCommand "codedown-powerline.conf" {} ''
   cat <<EOF >> powerline.conf
 # run-shell will print exit status on nonzero exit, so suppress by returning 0
 # (doesn't seem possible to change this)
-run-shell "PATH=\$PATH:${pythonWithPowerline}/bin ${pythonWithPowerline}/bin/powerline-daemon -q &> /dev/null; return 0"
+run-shell "PATH=\$PATH:${pythonWithPowerline}/bin:${sysstat}/bin ${pythonWithPowerline}/bin/powerline-daemon -q &> /dev/null; return 0"
 
 # It would be better to source powerline.conf, because it has a if-shell check to avoid running
 # "powerline-config tmux setup" unless necessary.
 # But, we have to make sure the PATH is set properly so it can find powerline-config, need to figure out
 # how to do that with tmux commands...
-run-shell "PATH=\$PATH:${pythonWithPowerline}/bin ${pythonWithPowerline}/bin/powerline-config tmux setup &> /dev/null; return 0"
+run-shell "PATH=\$PATH:${pythonWithPowerline}/bin:${sysstat}/bin ${pythonWithPowerline}/bin/powerline-config tmux setup &> /dev/null; return 0"
 # source ${powerline}/share/tmux/powerline.conf
 
 set-option -g default-terminal "screen-256color"
