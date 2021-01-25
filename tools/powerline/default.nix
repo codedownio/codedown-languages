f5d57a85afcc2495671fc509b4cb8ee3ce83f6ad
Tom McLaughlin <tom@codedown.io>
2021-01-25 08:51:41 -0800

    Suppress output from powerline commands

--- tools/powerline/default.nix
+++ tools/powerline/default.nix
@@ -30,9 +30,15 @@ runCommand "codedown-powerline.conf" {} ''
   cp ${./default.json} ./default.json
 
   cat <<EOF >> powerline.conf
-run-shell "PATH=\$PATH:${pythonWithPowerline}/bin ${pythonWithPowerline}/bin/powerline-daemon -q"
-run-shell "PATH=\$PATH:${pythonWithPowerline}/bin ${pythonWithPowerline}/bin/powerline-config tmux setup"
+run-shell "PATH=\$PATH:${pythonWithPowerline}/bin ${pythonWithPowerline}/bin/powerline-daemon -q &> /dev/null"
+
+# It would be better to source powerline.conf, because it has a if-shell check to avoid running
+# "powerline-config tmux setup" unless necessary.
+# But, we have to make sure the PATH is set properly so it can find powerline-config, need to figure out
+# how to do that with tmux commands...
+run-shell "PATH=\$PATH:${pythonWithPowerline}/bin ${pythonWithPowerline}/bin/powerline-config tmux setup &> /dev/null"
 # source ${powerline}/share/tmux/powerline.conf
+
 set-option -g default-terminal "screen-256color"
 EOF
 ''
