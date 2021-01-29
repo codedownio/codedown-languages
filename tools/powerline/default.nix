15a51ad9d3afe43617c7de9b8cbc59334c4cf6bc
Tom McLaughlin <tom@codedown.io>
2021-01-28 21:36:51 -0800

    Add powerline binaries to path

--- tools/powerline/default.nix
+++ tools/powerline/default.nix
@@ -23,7 +23,7 @@ let
 
 in
 
-runCommand "codedown-powerline.conf" {} ''
+runCommand "codedown-powerline" {} ''
   mkdir -p $out/share
   cd $out/share
 
@@ -32,15 +32,21 @@ runCommand "codedown-powerline.conf" {} ''
   cat <<EOF >> powerline.conf
 # run-shell will print exit status on nonzero exit, so suppress by returning 0
 # (doesn't seem possible to change this)
-run-shell "PATH=\$PATH:${pythonWithPowerline}/bin:${sysstat}/bin ${pythonWithPowerline}/bin/powerline-daemon -q &> /dev/null; return 0"
+run-shell "PATH=\$PATH:${pythonWithPowerline}/bin:${sysstat}/bin powerline-daemon -q &> /dev/null; return 0"
 
 # It would be better to source powerline.conf, because it has a if-shell check to avoid running
 # "powerline-config tmux setup" unless necessary.
 # But, we have to make sure the PATH is set properly so it can find powerline-config, need to figure out
 # how to do that with tmux commands...
-run-shell "PATH=\$PATH:${pythonWithPowerline}/bin:${sysstat}/bin ${pythonWithPowerline}/bin/powerline-config tmux setup &> /dev/null; return 0"
-# source ${powerline}/share/tmux/powerline.conf
+# run-shell "PATH=\$PATH:${pythonWithPowerline}/bin:${sysstat}/bin powerline-config tmux setup &> /dev/null; return 0"
+source ${powerline}/share/tmux/powerline.conf
 
 set-option -g default-terminal "screen-256color"
 EOF
+
+  mkdir -p $out/bin
+  cd $out/bin
+  for file in ${pythonWithPowerline}/bin/powerline*; do
+    ln -s $file $(basename $file)
+  done
 ''
