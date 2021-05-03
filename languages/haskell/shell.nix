05656fb8ff9977d2cbadc247cbd079afee68b205
Tom McLaughlin <tom@codedown.io>
2021-05-02 18:51:31 -0700

    Add example of haskell development shell

---
+++ languages/haskell/shell.nix
@@ -0,0 +1,22 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  haskellNix = import (builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz) {};
+  nixpkgs = import haskellNix.sources.nixpkgs haskellNix.nixpkgsArgs;
+  haskell = nixpkgs.haskell-nix;
+
+in
+    
+with pkgs;
+
+# haskell.haskellPackages.ghcWithPackages (ps: with ps; [ lens conduit conduit-extra ])
+
+mkShell {
+  buildInputs = [
+    (haskell.haskellPackages.ghcWithPackages (ps: with ps;
+      [ lens conduit conduit-extra aeson ]
+    ))
+
+    haskell.haskellPackages.ihaskell.components.exes.ihaskell
+  ];
+}
