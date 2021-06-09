43c80a316e32544d71e2bd41e1bc3803e3ae2511
Tom McLaughlin <tom@codedown.io>
2021-06-09 14:08:00 -0600

    name -> target in settings item

--- languages/python/default.nix
+++ languages/python/default.nix
@@ -51,7 +51,7 @@ lib.listToAttrs (map (x:
 
       settingsSchema = [
         {
-          name = "permitUserSite";
+          target = "permitUserSite";
           title = "Permit user site-packages";
           description = "Skip setting the PYTHONNOUSERSITE variable. This will allow your Python code to import local packages (e.g. from ~/.local/lib). This is useful if you want to use pip to install Python packages independently of Nix.";
           type = "boolean";
