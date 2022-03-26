39b0a51bf31902920bbd8e3ebcef308767af71ab
Tom McLaughlin <tom@codedown.io>
2022-03-26 16:45:12 -0700

    Remove unused let

--- default.nix
+++ default.nix
@@ -3,12 +3,6 @@ final: prev:
 with final;
 with final.lib;
 
-let
-  common = callPackage ./languages/common.nix {};
-  shellsCommon = callPackage ./shells/common.nix {};
-
-in
-
 rec {
   codedown = callPackage ./codedown.nix {};
 }
