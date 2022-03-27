d866004b4ed6c5e8fe5d651279c4fbab37582824
Tom McLaughlin <tom@codedown.io>
2022-03-26 17:34:52 -0700

    More flake

--- flake.nix
+++ flake.nix
@@ -8,13 +8,13 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+    flake-utils.lib.eachSystem (with flake-utils.lib.system; [ x86_64-linux ]) (system:
       let
         overlays = [ (import ./default.nix) ];
         pkgs = import nixpkgs { inherit system overlays; };
       in
         {
-          packages.x86_64-linux.languagesSearcher = pkgs.codedown.languagesSearcher;
+          languagesSearcher = (pkgs.callPackage ./codedown.nix {}).languagesSearcher;
           # packages.x86_64-linux.languagesSearcher2 = (pkgs.callPackage ./codedown.nix {}).languagesSearcher;
           # defaultPackage.x86_64-linux = self.packages.x86_64-linux.languagesSearcher;
         }
