f0bbc4f364a2794de0778e52dee4dc04cfc3b6e0
Tom McLaughlin <tom@codedown.io>
2022-03-28 14:51:31 -0700

    Fix old style builds

---
+++ old_environment.nix
@@ -0,0 +1,22 @@
+
+let
+  bootstrapNixpkgs = import <nixpkgs> {};
+  fetchgit = bootstrapNixpkgs.fetchgit;
+  fetchFromGitHub = bootstrapNixpkgs.fetchFromGitHub;
+  lib = bootstrapNixpkgs.lib;
+
+  overlays = {
+    codedown = {
+      tag = "path";
+      path = ./.;
+    };
+  };
+
+  channelsAll = with bootstrapNixpkgs; callPackage ./channels.nix { inherit overlays; };
+
+in
+
+bootstrapNixpkgs.callPackage ./environment.nix {
+  inherit overlays;
+  inherit (channelsAll) channels importedChannels importedOverlays;
+}
