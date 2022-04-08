a47e525088130e6f3780b557642a14ae525c19e4
Tom McLaughlin <tom@codedown.io>
2022-04-08 12:32:45 -0700

    Try exposing ghc-boot-packages

--- default.nix
+++ default.nix
@@ -12,17 +12,19 @@ let
     rev = "110a2c9ebbf5d4a94486854f18a37a938cfacbbb";
     sha256 = "0v12ylqxy1kl06dgln6h5k8vhlfzp8xvdymljj7bl0avr0nrgrcm";
   };
+
   haskellNix = import (fetchFromGitHub {
-    owner = "input-output-hk";
+    owner = "codedownio";
     repo = "haskell.nix";
-    rev = "4cda3aeea8acd0837f6ad4a18a793d3d5901862a";
-    sha256 = "sha256-D9xRMBhsjHi5Ox2SsnzlwgcMXZlAXWP5c/AqFY0kxl4=";
+    rev = "a3acfa49682870cc1db1120bd0c5f4a593de5884";
+    sha256 = "1hxx57zcffi3ajibqrm9y3sssp8bwbdr0fyhr2lrvmzvv0k8n6ci";
   }) { pkgs = import nixpkgsSrc {}; };
 
 in
 
 rec {
   haskell-nix = (import nixpkgsSrc haskellNix.nixpkgsArgs).haskell-nix;
+  ghc-boot-packages = (import nixpkgsSrc haskellNix.nixpkgsArgs).ghc-boot-packages;
 
   codedown = callPackage ./codedown.nix {};
 }
