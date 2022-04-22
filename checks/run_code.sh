b0ea6c9f85978ba730d2ffd02e4b63dfb30058d2
Tom McLaughlin <tom@codedown.io>
2022-04-21 19:23:45 -0700

    Simpler run_code.sh

--- checks/run_code.sh
+++ checks/run_code.sh
@@ -2,29 +2,32 @@
 
 cd "$(mktemp -d)"
 
-echo "Using temp dir: $(pwd)"
+>&2 echo "Using temp dir: $(pwd)"
 
 KERNEL="$1"
 CODE="$2"
 
-cat <<EOF > notebook.ipynb
-{
-    "nbformat_minor": 2,
-    "nbformat": 4,
-    "cells": [
-        {
-            "metadata": {},
-            "source": ["$CODE"],
-            "cell_type": "code",
-            "execution_count": null,
-            "outputs": []
-        }
-    ],
-    "metadata": {}
-}
-EOF
+# cat <<EOF > notebook.ipynb
+# {
+#     "nbformat_minor": 2,
+#     "nbformat": 4,
+#     "cells": [
+#         {
+#             "metadata": {},
+#             "source": ["$CODE"],
+#             "cell_type": "code",
+#             "execution_count": null,
+#             "outputs": []
+#         }
+#     ],
+#     "metadata": {}
+# }
+# EOF
 
 
-jupyter execute --kernel "$KERNEL" notebook.ipynb
+# jupyter execute --kernel "$KERNEL" notebook.ipynb
+# jupyter run --kernel "$KERNEL" notebook.ipynb
+# jq '.cells[0].outputs' < notebook.ipynb
 
-jq '.cells[0].outputs' < notebook.ipynb
+
+echo "$CODE" | jupyter run --kernel "$KERNEL"
