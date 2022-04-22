310b7b895c972472de53c061243b5608e552ccef
Tom McLaughlin <tom@codedown.io>
2022-04-22 16:20:44 -0700

    Break up checks

--- checks.nix
+++ checks.nix
@@ -1,42 +1,4 @@
 {
-  python = {
-    kernels = [({
-      channel = "nixpkgs-unstable";
-      language = "python38";
-      args = {
-        packages = ["matplotlib" "scipy" "rope"];
-        languageServers = ["jedi" "pyright" "pylint" "flake8" "pycodestyle" "microsoft" "python-lsp-server" "python-language-server"];
-        settings = {
-          permitUserSite = false;
-        };
-      };
-    })];
-
-    codeExecutions = map (x: x // { kernel = "python38"; }) [{
-      code = "print(42)";
-      output = "42";
-    } {
-      code = "import scipy";
-      output = "";
-    }];
-  };
-
-  haskell = {
-    kernels = [({
-      channel = "nixpkgs";
-      language = "haskell-ghc902";
-      args = {
-        packages = ["aeson"];
-        languageServers = ["haskell-language-server"];
-      };
-    })];
-
-    codeExecutions = map (x: x // { kernel = "haskell-ghc902"; }) [{
-      code = ''putStrLn "hi"'';
-      output = "hi";
-    } {
-      code = "import Data.Aeson";
-      output = "";
-    }];
-  };
+  haskell = import ./checks/haskell.nix;
+  python = import ./checks/python.nix;
 }
---
+++ checks/haskell.nix
@@ -0,0 +1,18 @@
+{
+  kernels = [({
+    channel = "nixpkgs";
+    language = "haskell-ghc902";
+    args = {
+      packages = ["aeson"];
+      languageServers = ["haskell-language-server"];
+    };
+  })];
+
+  codeExecutions = map (x: x // { kernel = "haskell-ghc902"; }) [{
+    code = ''putStrLn "hi"'';
+    output = "hi";
+  } {
+    code = "import Data.Aeson";
+    output = "";
+  }];
+}
