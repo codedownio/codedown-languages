647e238096d14d44043e91e04182f73de9d9791c
Tom McLaughlin <tom@codedown.io>
2022-04-24 19:19:01 -0700

    Add another GHC versions to haskell checks

--- checks/haskell.nix
+++ checks/haskell.nix
@@ -1,18 +1,34 @@
-{
-  kernels = [({
-    channel = "nixpkgs-unstable";
-    language = "haskell-ghc902";
-    args = {
-      packages = ["aeson"];
-      languageServers = ["haskell-language-server"];
-    };
-  })];
 
-  codeExecutions = map (x: x // { kernel = "haskell-ghc902"; }) [{
+let
+  code = [{
     code = ''putStrLn "hi"'';
     output = "hi";
   } {
     code = "import Data.Aeson";
     output = "";
   }];
+
+in
+
+{
+  kernels = [{
+    channel = "nixpkgs-unstable";
+    language = "haskell-ghc902";
+    args = {
+      packages = ["aeson"];
+      languageServers = ["haskell-language-server"];
+    };
+  } {
+    channel = "nixpkgs-unstable";
+    language = "haskell-ghc8107";
+    args = {
+      packages = ["aeson"];
+      languageServers = ["haskell-language-server"];
+    };
+  }];
+
+  codeExecutions =
+    (map (x: x // { kernel = "haskell-ghc902"; }) code)
+    ++ (map (x: x // { kernel = "haskell-ghc8107"; }) code)
+  ;
 }
