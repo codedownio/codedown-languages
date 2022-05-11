0b092fb57202235e6239a3a9777187f2cec462ba
Tom McLaughlin <tom@codedown.io>
2022-05-11 16:26:26 -0700

    Try setting flake8 ignores for pythonlsp

--- languages/python/language_server_pythonlsp/config.nix
+++ languages/python/language_server_pythonlsp/config.nix
@@ -54,4 +54,7 @@ common.writeTextDirWithMeta python.pkgs.python-language-server.meta "lib/codedow
     attrs = ["python"];
     type = "tcp";
     args = ["${pythonEnv}/bin/python" "-m" "pyls" "--tcp" "--host=localhost" "--port={port_number}"];
+    initialization_options = {
+      "pylsp.plugins.flake8.ignore" = ["E303" "E402"]
+    }
   }])
