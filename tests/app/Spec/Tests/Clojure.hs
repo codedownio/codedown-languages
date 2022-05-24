7ae8989de7b4c563b51dcc48814c04029481ae0a
Tom McLaughlin <tom@codedown.io>
2022-05-24 12:06:44 -0700

    Clojure test

---
+++ tests/app/Spec/Tests/Clojure.hs
@@ -0,0 +1,30 @@
+
+module Spec.Tests.Clojure (tests) where
+
+import Data.String.Interpolate
+import Test.Sandwich as Sandwich
+import TestLib.JupyterRunnerContext
+import TestLib.NixEnvironmentContext
+import TestLib.NixTypes
+
+
+kernelSpec = NixKernelSpec {
+  nixKernelChannel = "codedown"
+  , nixKernelLanguage = "clojure"
+  , nixKernelDisplayName = Just "Clojure"
+  , nixKernelPackages = []
+  , nixKernelLanguageServers = []
+  , nixKernelExtraJupyterConfig = Nothing
+  , nixKernelMeta = Nothing
+  , nixKernelIcon = Nothing
+  , nixKernelSettingsSchema = Nothing
+  , nixKernelSettings = Nothing
+  }
+
+tests :: TopSpec
+tests = describe "Clojure" $ introduceNixEnvironment [kernelSpec] [] "Clojure" $ introduceJupyterRunner $ do
+  testKernelStdout "clojure" [__i|(println "hi")|] "hi\n"
+
+
+main :: IO ()
+main = runSandwichWithCommandLineArgs Sandwich.defaultOptions tests
