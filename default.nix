f00b6d67468ea81d3d58b02490a7136a32ae0efe
Tom McLaughlin <tom@codedown.io>
2022-06-09 17:56:43 -0700

    Bring over some flake changes

--- default.nix
+++ default.nix
@@ -2,12 +2,10 @@
 }:
 
 let
-  # This must be chosen to match haskellNix.sources.nixpkgs!
-  # We do it ourselves because we want to use fetchFromGitHub instead of fetchTarball.
   nixpkgsSrc = fetchFromGitHub {
     owner = "NixOS";
     repo = "nixpkgs";
-    rev = "ce6aa13369b667ac2542593170993504932eb836";
+    rev = "ce6aa13369b667ac2542593170993504932eb836"; # 22.05
     sha256 = "0d643wp3l77hv2pmg2fi7vyxn4rwy0iyr8djcw1h5x72315ck9ik";
   };
 
--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   description = "CodeDown languages";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/973910f5c31b9ba6c171c33a8bd7199990b14c72";
-  inputs.nixpkgs-unstable.url = "github:NixOS/nixpkgs/ecaf3da9340231e5493eccc3db87604a3705da42";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/ce6aa13369b667ac2542593170993504932eb836";
+  inputs.nixpkgs-unstable.url = "github:NixOS/nixpkgs/18de53ca965bd0678aaf09e5ce0daae05c58355a";
 
   # When updating this, make sure to run ./update.sh to generate the new compiler set!
   inputs.haskellNixSrc.url = "github:input-output-hk/haskell.nix/f3ea06dcacc8a46b4a207a6a1fad14bc5ea41b19";
@@ -15,71 +15,28 @@
     # flake-utils.lib.eachDefaultSystem (system:
     flake-utils.lib.eachSystem ["x86_64-linux"] (system:
       let
-        baseNixpkgs = import nixpkgs { inherit system; };
         overlays = [
           haskellNixSrc.outputs.overlay (final: prev: {
             inherit (pkgs.lib.getAttr system ihaskell.packages) ihaskell-884 ihaskell-8107 ihaskell-902 ihaskell-921;
-            codedown = final.callPackage ./codedown.nix {};
           })
         ];
 
         pkgs = import nixpkgs { inherit system overlays; };
         pkgsUnstable = import nixpkgs-unstable { inherit system overlays; };
 
-        callEnvironment = path: args: pkgs.callPackage path (rec {
-          channels = pkgs.lib.listToAttrs (map (x: {
-            name = x;
-            value = {
-              tag = "fetch_from_github";
-              owner = "NixOS";
-              repo = "nixpkgs";
-              rev = inputs.${x}.rev;
-              sha256 = inputs.${x}.narHash;
-            };
-          }) ["nixpkgs" "nixpkgs-unstable"]);
-
-          # importedChannels = pkgs.lib.listToAttrs (map (x: {
-          #   name = x;
-          #   value = import inputs.${x} { inherit system overlays; };
-          # }) ["nixpkgs" "nixpkgs-unstable"]);
-          importedChannels = { nixpkgs = pkgs; nixpkgs-unstable = pkgsUnstable; };
-
-          overlays = {
-            codedown = {
-              tag = "path";
-              path = ./default_old.nix;
-            };
-          };
-          importedOverlays = pkgs.lib.mapAttrsToList (name: value: import (channelSpecToChannel name value)) overlays;
-        } // args);
-
         channelSpecToChannel = name: channel:
           if (channel.tag == "fetch_from_github") then pkgs.fetchFromGitHub ((removeAttrs channel ["tag" "name"]))
           else if (channel.tag == "fetch_git") then pkgs.fetchgit (removeAttrs channel ["tag" "name"])
           else if (channel.tag == "path") then channel.path else null;
+
+        callEnvironment = (pkgs.callPackage ./util.nix { inherit pkgsUnstable channelSpecToChannel inputs; }).callEnvironment;
+
+        codedown = pkgs.callPackage ./codedown.nix {};
+
       in
         rec {
-          checks = let checks = with pkgs.lib; import ./checks.nix; in (
-            pkgs.lib.listToAttrs (pkgs.lib.flatten ((pkgs.lib.mapAttrsToList (n: v: [{
-              name = n + "-build-environment";
-              value = callEnvironment ./empty_environment.nix { inherit (v) kernels; };
-            } {
-              name = n + "-run-code";
-              value = pkgs.callPackage ./check_code.nix {
-                inherit (v) codeExecutions;
-                jupyter_path = "${callEnvironment ./empty_environment.nix { inherit (v) kernels; }}/lib/codedown";
-              };
-            }])) checks))
-          );
-
-          packages = rec {
-            exportersSearcher = pkgs.codedown.exportersSearcher;
-            shellsSearcher = pkgs.codedown.shellsSearcher;
-            languagesSearcher = pkgs.codedown.languagesSearcher;
-
+          packages = codedown // {
             haskellCompilers = pkgs.callPackage ./languages/haskell/generate.nix {};
-
-            codedown = pkgs.codedown;
 
             jupyter-runner = with pkgs; let
               pythonEnv = python38.withPackages (ps: with ps; [papermill]);
@@ -90,9 +47,7 @@
                     --set PATH ${lib.makeBinPath packages}
                 '';
 
-            environment = callEnvironment ./environment.nix {};
-
-            ci = pkgs.callPackage ./ci.nix { inherit checks; };
+            environment = callEnvironment ./environment.nix { inherit codedown; };
           };
         }
     );
