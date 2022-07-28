043de4e05810fd6002ec267693ff58d4bde20c7b
Tom McLaughlin <tom@codedown.io>
2022-07-27 18:37:01 -0700

    go/language-server.nix

---
+++ languages/go/language-server.nix
@@ -0,0 +1,25 @@
+{ config
+, lib
+, pkgs
+, kernelName
+, go-langserver
+}:
+
+let
+  common = callPackage ../../common.nix {};
+
+in
+
+common.writeTextDirWithMeta go-langserver.meta "lib/codedown/go-language-servers.yaml" (lib.generators.toYAML {} [{
+  name = "go-langserver";
+  display_name = "go-langserver";
+  description = go-langserver.meta.description;
+  icon = ./logo-64x64.png;
+  extensions = ["go"];
+  notebook_suffix = ".go";
+  kernel_name = kernelName;
+  attrs = ["go"];
+  type = "stream";
+  args = ["${go-langserver}/bin/go-langserver"];
+  env = {};
+}])
