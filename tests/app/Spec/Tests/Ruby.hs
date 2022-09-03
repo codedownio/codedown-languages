35da9fa1954280d48602222cdf3603df69439002
Tom McLaughlin <tom@codedown.io>
2022-09-02 18:00:01 -0700

    Check in ruby test

---
+++ tests/app/Spec/Tests/Ruby.hs
@@ -0,0 +1,30 @@
+
+module Spec.Tests.Ruby (tests) where
+
+import Data.String.Interpolate
+import Test.Sandwich as Sandwich
+import TestLib.JupyterRunnerContext
+import TestLib.LSP
+import TestLib.NixEnvironmentContext
+import TestLib.NixTypes
+
+
+kernelSpec = NixKernelSpec {
+  nixKernelChannel = "codedown"
+  , nixKernelLanguage = "ruby"
+  , nixKernelDisplayName = Just "Ruby"
+  , nixKernelPackages = []
+  , nixKernelLanguageServers = []
+  , nixKernelExtraJupyterConfig = Nothing
+  , nixKernelMeta = Nothing
+  , nixKernelIcon = Nothing
+  , nixKernelSettingsSchema = Nothing
+  , nixKernelSettings = Nothing
+  }
+
+tests :: TopSpec
+tests = describe "Ruby" $ introduceNixEnvironment [kernelSpec] [] "Ruby" $ introduceJupyterRunner $ do
+  testKernelStdout "ruby" [__i|puts "hi"|] "hi\n"
+
+main :: IO ()
+main = runSandwichWithCommandLineArgs Sandwich.defaultOptions tests
