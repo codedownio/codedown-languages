861e312d67d7cee2808ab8c92251f5c92bf6f27a
Tom McLaughlin <tom@codedown.io>
2022-11-28 18:03:45 -0700

    Regenerate depot_18

--- languages/julia/depot_18/default.nix
+++ languages/julia/depot_18/default.nix
@@ -1,19 +1,22 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> {}
+
+# The base Julia version
+, baseJulia ? pkgs.julia-stable-bin
+
+# Extra libraries for Julia's LD_LIBRARY_PATH.
+# Recent Julia packages that use Artifacts.toml to specify their dependencies
+# shouldn't need this.
+# But if a package implicitly depends on some library being present at runtime, you can
+# add it here.
+, extraLibs ? []
+
+, python ? pkgs.python3
+}:
 
 with pkgs;
 
 
 let
-  # The base Julia version
-  baseJulia = julia-lts-bin;
-
-  # Extra libraries for Julia's LD_LIBRARY_PATH.
-  # Recent Julia packages that use Artifacts.toml to specify their dependencies
-  # shouldn't need this.
-  # But if a package implicitly depends on some library being present at runtime, you can
-  # add it here.
-  extraLibs = [];
-
   # Wrapped Julia with libraries and environment variables.
   # Note: setting The PYTHON environment variable is recommended to prevent packages
   # from trying to obtain their own with Conda.
@@ -21,7 +24,7 @@ let
     mkdir -p $out/bin
     makeWrapper ${baseJulia}/bin/julia $out/bin/julia \
                 --suffix LD_LIBRARY_PATH : "${lib.makeLibraryPath extraLibs}" \
-                --set PYTHON ${python3}/bin/python
+                --set PYTHON ${python}/bin/python
   '';
 
 in
@@ -31,9 +34,6 @@ callPackage ./common.nix {
 
   # Run Pkg.precompile() to precompile all packages?
   precompile = true;
-
-  # Package Julia with a startup file to automatically activate the constructed environment.
-  autoActivate = false;
 
   # Extra arguments to makeWrapper when creating the final Julia wrapper.
   # By default, it will just put the new depot at the end of JULIA_DEPOT_PATH.
