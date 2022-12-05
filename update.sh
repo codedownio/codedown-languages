300e02aa1fcbfc59813dd25406cd04181731a1df
Tom McLaughlin <tom@codedown.io>
2022-12-05 02:30:00 -0800

    Remove old script

--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eo pipefail
-
-SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $SCRIPTDIR
-
-cp $(nix build --no-link .#haskellCompilers --json | jq -r '.[0].outputs.out') ./languages/haskell/compilers.nix
-chmod 664 ./languages/haskell/compilers.nix
