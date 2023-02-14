9a3cb22630aa1549e1d91401126a1e3e77a91dfe
Tom McLaughlin <tom@codedown.io>
2023-02-13 17:37:56 -0800

    Try using builtins.fetchGit in default.nix

--- default.nix
+++ default.nix
@@ -1,19 +1,15 @@
-{ fetchFromGitHub
-}:
 
 let
-  pkgsStable = import (fetchFromGitHub {
-    owner = "NixOS";
-    repo = "nixpkgs";
+  pkgsStable = import (builtins.fetchGit {
+    url = "https://github.com/NixOS/nixpkgs";
     rev = "6d3fc36c541ae715d43db5c1355890f39024b26f"; # nixpkgs-rev
-    sha256 = "sha256-cRsIC0Ft5McBSia0rDdJIHy3muWqKn3rvjFx92DU2dY="; # nixpkgs-sha256
+    ref = "release-22.11";
   }) {};
 
-  pkgsUnstable = import (fetchFromGitHub {
-    owner = "NixOS";
-    repo = "nixpkgs";
+  pkgsUnstable = import (builtins.fetchGit {
+    url = "https://github.com/NixOS/nixpkgs";
     rev = "a2d2f70b82ada0eadbcb1df2bca32d841a3c1bf1"; # nixpkgs-unstable-rev
-    sha256 = "sha256-EcDfOiTHs0UBAtyGc0wxJJdhcMjrJEgWXjJutxZGA3E="; # nixpkgs-unstable-sha256
+    ref = "nixpkgs-unstable";
   }) {};
 
 in
