105eff4288f56b1b416a228d5c8e0aaae706050a
Tom McLaughlin <tom@codedown.io>
2023-02-13 21:57:03 -0800

    Revert "Try using builtins.fetchGit in default.nix"
    This reverts commit 9a3cb22630aa1549e1d91401126a1e3e77a91dfe.

--- default.nix
+++ default.nix
@@ -1,15 +1,19 @@
+{ fetchFromGitHub
+}:
 
 let
-  pkgsStable = import (builtins.fetchGit {
-    url = "https://github.com/NixOS/nixpkgs";
+  pkgsStable = import (fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
     rev = "6d3fc36c541ae715d43db5c1355890f39024b26f"; # nixpkgs-rev
-    ref = "release-22.11";
+    sha256 = "sha256-cRsIC0Ft5McBSia0rDdJIHy3muWqKn3rvjFx92DU2dY="; # nixpkgs-sha256
   }) {};
 
-  pkgsUnstable = import (builtins.fetchGit {
-    url = "https://github.com/NixOS/nixpkgs";
+  pkgsUnstable = import (fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
     rev = "a2d2f70b82ada0eadbcb1df2bca32d841a3c1bf1"; # nixpkgs-unstable-rev
-    ref = "nixpkgs-unstable";
+    sha256 = "sha256-EcDfOiTHs0UBAtyGc0wxJJdhcMjrJEgWXjJutxZGA3E="; # nixpkgs-unstable-sha256
   }) {};
 
 in
