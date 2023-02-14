ef3ba234b474071d76fb784b7d6b070883a79a4a
Tom McLaughlin <tom@codedown.io>
2023-02-13 21:56:52 -0800

    Revert "More with builtins.fetchGit"
    This reverts commit d338dc6be16a8481156013d2d944652dc535945b.

--- default.nix
+++ default.nix
@@ -4,17 +4,14 @@ let
     url = "https://github.com/NixOS/nixpkgs";
     rev = "6d3fc36c541ae715d43db5c1355890f39024b26f"; # nixpkgs-rev
     ref = "release-22.11";
-    shallow = true;
+  }) {};
+
+  pkgsUnstable = import (builtins.fetchGit {
+    url = "https://github.com/NixOS/nixpkgs";
+    rev = "a2d2f70b82ada0eadbcb1df2bca32d841a3c1bf1"; # nixpkgs-unstable-rev
+    ref = "nixpkgs-unstable";
   }) {};
 
 in
 
-pkgsStable.callPackage ./codedown.nix {
-  inherit pkgsStable;
-  pkgsUnstable = pkgsStable.fetchFromGitHub {
-    owner = "NixOS";
-    repo = "nixpkgs";
-    rev = "a2d2f70b82ada0eadbcb1df2bca32d841a3c1bf1"; # nixpkgs-unstable-rev
-    sha256 = "sha256-EcDfOiTHs0UBAtyGc0wxJJdhcMjrJEgWXjJutxZGA3E="; # nixpkgs-unstable-sha256
-  };
-}
+pkgsStable.callPackage ./codedown.nix { inherit pkgsStable pkgsUnstable; }
