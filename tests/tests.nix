45654ddbd2ad2fe6535e901d4525ee3e35f48b0e
Tom McLaughlin <tom@codedown.io>
2023-04-23 02:17:37 -0700

    Generate tests nix

--- tests/tests.nix
+++ tests/tests.nix
@@ -1,10 +1,10 @@
-{ mkDerivation, aeson, aeson-qq, base, bytestring, containers
-, data-default, directory, exceptions, filepath, hashable, hpack
-, lens, lib, lsp-test, lsp-types, monad-control, monad-logger, mtl
-, network, optparse-applicative, postgresql-libpq
-, postgresql-simple, process, random, resource-pool, retry, safe
-, sandwich, string-interpolate, text, unliftio, unliftio-core
-, unordered-containers, vector
+{ mkDerivation, aeson, base, bytestring, conduit, conduit-aeson
+, containers, data-default, directory, exceptions, filepath
+, hashable, lens, lib, lsp-test, lsp-types, monad-control
+, monad-logger, mtl, network, optparse-applicative
+, postgresql-libpq, postgresql-simple, process, random
+, resource-pool, retry, safe, sandwich, string-interpolate
+, temporary, text, time, unliftio, unliftio-core, vector
 }:
 mkDerivation {
   pname = "tests";
@@ -13,23 +13,19 @@ mkDerivation {
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    aeson aeson-qq base bytestring containers data-default directory
-    exceptions filepath hashable lens lsp-test lsp-types monad-control
-    monad-logger mtl network optparse-applicative postgresql-libpq
+    aeson base bytestring conduit conduit-aeson containers data-default
+    directory exceptions filepath hashable lens lsp-test lsp-types
+    monad-control monad-logger mtl network postgresql-libpq
     postgresql-simple process random resource-pool retry safe sandwich
-    string-interpolate text unliftio unliftio-core unordered-containers
+    string-interpolate temporary text time unliftio unliftio-core
     vector
   ];
-  libraryToolDepends = [ hpack ];
   executableHaskellDepends = [
-    aeson aeson-qq base bytestring containers data-default directory
-    exceptions filepath hashable lens lsp-test lsp-types monad-control
-    monad-logger mtl network optparse-applicative postgresql-libpq
-    postgresql-simple process random resource-pool retry safe sandwich
-    string-interpolate text unliftio unliftio-core unordered-containers
-    vector
+    aeson base bytestring containers exceptions hashable lens lsp-test
+    lsp-types monad-control optparse-applicative sandwich
+    string-interpolate text unliftio-core vector
   ];
   executableToolDepends = [ sandwich ];
-  prePatch = "hpack";
   license = lib.licenses.bsd3;
+  mainProgram = "tests-exe";
 }
