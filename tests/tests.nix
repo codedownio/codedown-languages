0baa92a39fdc4dea16bd631479beecaaa211ec7d
Tom McLaughlin <tom@codedown.io>
2023-07-12 21:16:01 -0700

    Regen tests

--- tests/tests.nix
+++ tests/tests.nix
@@ -2,9 +2,9 @@
 , containers, data-default, directory, exceptions, filepath, lens
 , lib, lsp-test, lsp-types, monad-control, monad-logger, mtl
 , network, optparse-applicative, postgresql-libpq
-, postgresql-simple, process, random, resource-pool, retry, safe
-, sandwich, string-interpolate, temporary, text, time, unliftio
-, unliftio-core, vector
+, postgresql-simple, process, random, resource-pool, retry
+, row-types, safe, sandwich, string-interpolate, temporary, text
+, time, unliftio, unliftio-core, vector
 }:
 mkDerivation {
   pname = "tests";
@@ -16,13 +16,14 @@ mkDerivation {
     aeson base bytestring conduit conduit-aeson containers data-default
     directory exceptions filepath lens lsp-test lsp-types monad-control
     monad-logger mtl network postgresql-libpq postgresql-simple process
-    random resource-pool retry safe sandwich string-interpolate
-    temporary text time unliftio unliftio-core vector
+    random resource-pool retry row-types safe sandwich
+    string-interpolate temporary text time unliftio unliftio-core
+    vector
   ];
   executableHaskellDepends = [
     aeson base bytestring containers exceptions lens lsp-test lsp-types
     monad-control optparse-applicative safe sandwich string-interpolate
-    text unliftio-core vector
+    text unliftio unliftio-core vector
   ];
   executableToolDepends = [ sandwich ];
   license = lib.licenses.bsd3;
