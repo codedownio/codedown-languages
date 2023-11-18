059bf393014f60429e4367233684f80b11b6115e
Tom McLaughlin <tom@codedown.io>
2023-11-17 19:37:02 -0800

    Add Haskell DocumentHighlight test in raw file

--- tests/app/Spec/Tests/Haskell/DocumentHighlight.hs
+++ tests/app/Spec/Tests/Haskell/DocumentHighlight.hs
@@ -1,5 +1,10 @@
 
-module Spec.Tests.Haskell.DocumentHighlight where
+module Spec.Tests.Haskell.DocumentHighlight (
+  documentHighlightTests
+
+  , documentHighlightCode
+  , documentHighlightResults
+  ) where
 
 import Data.String.Interpolate
 import Data.Text as T
@@ -8,13 +13,23 @@ import Language.LSP.Test hiding (message)
 import Spec.Tests.Haskell.Common
 import Test.Sandwich as Sandwich
 import TestLib.LSP
+import TestLib.NixEnvironmentContext
 
 
 documentHighlightTests :: (LspContext context m) => SpecFree context m ()
 documentHighlightTests = describe "Document highlight" $ do
   it "highlights foo" $ doNotebookSession lsName documentHighlightCode $ \filename -> do
     ident <- openDoc filename "haskell"
-    getHighlights ident (Position 0 1) >>= (`shouldBe`  documentHighlightResults)
+    getHighlights ident (Position 0 1) >>= (`shouldBe` documentHighlightResults)
+
+  it "highlights foo in a regular doc" $ doSession' "Test.hs" lsName documentHighlightCodeRegular $ \filename -> do
+    ident <- openDoc filename "haskell"
+    getHighlights ident (Position 0 1) >>= (
+      `shouldBe`  [
+          DocumentHighlight (Range (Position 0 0) (Position 0 3)) (Just DocumentHighlightKind_Write)
+          , DocumentHighlight (Range (Position 2 11) (Position 2 14)) (Just DocumentHighlightKind_Read)
+          ]
+      )
 
 documentHighlightCode :: Text
 documentHighlightCode = [__i|foo = "hello"
@@ -25,3 +40,15 @@ documentHighlightResults = [
   DocumentHighlight (Range (Position 0 0) (Position 0 3)) (Just DocumentHighlightKind_Write)
   , DocumentHighlight (Range (Position 1 9) (Position 1 12)) (Just DocumentHighlightKind_Read)
   ]
+
+documentHighlightCodeRegular :: Text
+documentHighlightCodeRegular = [__i|foo = "hello"
+                                    main = do
+                                      putStrLn foo|]
+
+-------------------------------------
+
+main :: IO ()
+main = runSandwichWithCommandLineArgs Sandwich.defaultOptions $ do
+  introduceNixEnvironment [kernelSpec "haskell-ghc92"] [] "Haskell" $ do
+    documentHighlightTests
