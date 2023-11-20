c09f15d3a332e19b9a3dea056226320a13131045
thomasjm <tom@codedown.io>
2023-11-20 13:18:34 -0800

    Fix ghc94 build

--- languages/haskell/compilers.nix
+++ languages/haskell/compilers.nix
@@ -6,6 +6,19 @@
 , ihaskell-source
 }:
 
+let
+  ghc-syntax-highlighter_0_0_10_0 = callCabal2nix: let
+    src = fetchFromGitHub {
+      owner = "mrkkrp";
+      repo = "ghc-syntax-highlighter";
+      rev = "71ff751eaa6034d4aef254d6bc5a8be4f6595344";
+      sha256 = "wQmWSuvIJpg11zKl1qOSWpqxjp2DoJwa20vaS2KHypM=";
+    };
+  in
+    callCabal2nix "ghc-syntax-highlighter" src {};
+
+in
+
 {
   # ghc810 = haskell.packages.ghc810.override {
   #   overrides = self: super: {
@@ -71,15 +84,8 @@
 
       ihaskell = self.callCabal2nixWithOptions "ihaskell" ihaskell-source "--no-check" {};
 
-      ghc-syntax-highlighter = let
-        src = fetchFromGitHub {
-          owner = "mrkkrp";
-          repo = "ghc-syntax-highlighter";
-          rev = "bbc049904524aae08e6431494f41fe2a288f6259";
-          sha256 = "sha256-w7AxGsUfqGhh7wrSPppQ2+gPwjvb4mwExJdDOcasAZ4=";
-        };
-      in
-        self.callCabal2nix "ghc-syntax-highlighter" src {};
+      ghc-lib-parser = self.ghc-lib-parser_9_6_3_20231014;
+      ghc-syntax-highlighter = ghc-syntax-highlighter_0_0_10_0 self.callCabal2nix;
 
       zeromq4-haskell = super.zeromq4-haskell.overrideAttrs (oldAttrs: {
         buildInputs = oldAttrs.buildInputs ++ [libsodium];
@@ -103,15 +109,7 @@
       ghc-lib-parser = self.ghc-lib-parser_9_6_3_20231014;
       ghc-lib-parser-ex = self.ghc-lib-parser-ex_9_6_0_2;
 
-      ghc-syntax-highlighter = let
-        src = fetchFromGitHub {
-          owner = "mrkkrp";
-          repo = "ghc-syntax-highlighter";
-          rev = "71ff751eaa6034d4aef254d6bc5a8be4f6595344";
-          sha256 = "wQmWSuvIJpg11zKl1qOSWpqxjp2DoJwa20vaS2KHypM=";
-        };
-      in
-        self.callCabal2nix "ghc-syntax-highlighter" src {};
+      ghc-syntax-highlighter = ghc-syntax-highlighter_0_0_10_0 self.callCabal2nix;
 
       zeromq4-haskell = super.zeromq4-haskell.overrideAttrs (oldAttrs: {
         buildInputs = oldAttrs.buildInputs ++ [libsodium];
