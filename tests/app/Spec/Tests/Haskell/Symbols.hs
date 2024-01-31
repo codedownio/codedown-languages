54e9ab8a8a011942b5df09f1cd9b493d5d7e484c
thomasjm <tom@codedown.io>
2024-01-31 08:50:02 -0800

    Try adding a timeout to Haskell symbols test

--- tests/app/Spec/Tests/Haskell/Symbols.hs
+++ tests/app/Spec/Tests/Haskell/Symbols.hs
@@ -8,11 +8,12 @@ import Spec.Tests.Haskell.Common
 import Spec.Tests.Haskell.DocumentHighlight (documentHighlightCode)
 import Test.Sandwich as Sandwich
 import TestLib.LSP
+import UnliftIO.Timeout
 
 
 symbolsTests :: (LspContext context m) => SpecFree context m ()
 symbolsTests = describe "Symbols" $ do
   it "symbols" $ doNotebookSession lsName documentHighlightCode $ \filename -> do
     ident <- openDoc filename "haskell"
-    Right documentSymbols <- getDocumentSymbols ident
+    Just (Right documentSymbols) <- timeout 300_000_000 $ getDocumentSymbols ident
     fmap (^. name) documentSymbols `shouldBe` ["foo"]
