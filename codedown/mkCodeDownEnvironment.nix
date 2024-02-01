b3bf7c1178579319ffb11ca7ad8db02e7db5769f
Tom McLaughlin <tom@codedown.io>
2024-01-31 19:01:18 -0800

    Move UI metadata helpers to their own file noci

--- codedown/mkCodeDownEnvironment.nix
+++ codedown/mkCodeDownEnvironment.nix
@@ -48,54 +48,7 @@ let
     ++ concatMap (kernel: lib.mapAttrsToList (name: value: value // { inherit name; }) (if kernel.passthru ? "repls" then kernel.passthru.repls else {})) builtKernels
   ;
 
-  chooseInterestingMeta = lib.filterAttrs (n: v:
-    n == "description"
-    || n == "homepage"
-    || n == "changelog"
-    || (n == "available" && !v)
-    || (n == "broken" && v)
-    || (n == "unfree" && v)
-    || (n == "unsupported" && v)
-    || (n == "insecure" && v)
-    # || (n == "license.spdxId") # TODO
-  );
-
-  mkChannelUiMetadata = name: channel: {
-    foo = "bar";
-  };
-
-  mkKernelUiMetadata = kernel: {
-    # Dry
-    channel = kernel.channel;
-    name = kernel.name;
-    packages = kernel.args.packages;
-    settings = if kernel ? "settings" then kernel.settings else {};
-
-    # Hydrated
-    display_name = "todo";
-    icon = if kernel.meta ? "icon" then kernel.meta.icon else null;
-    modes = kernel.modes;
-    settings_schema = if kernel ? "settingsSchema" then kernel.settingsSchema else {};
-
-    # TODO?
-    languageServerNames = [];
-  };
-
-  mkOtherPackageUiMetadata = package: {
-    channel = package.channel;
-    attr = package.attr;
-    meta = if package.contents ? "meta" then chooseInterestingMeta package.contents.meta else {};
-  };
-
-  ui_metadata = {
-    channels = lib.mapAttrsToList mkChannelUiMetadata channels;
-
-    kernels = map mkKernelUiMetadata builtKernels;
-
-    other_packages = map mkOtherPackageUiMetadata otherPackages;
-  };
-
-  ui_metadata_yaml = writeText "ui-metadata.yaml" (lib.generators.toYAML {} ui_metadata);
+  uiMetadata = callPackage ./uiMetadata.nix {};
 
 in
 
@@ -110,7 +63,15 @@ symlinkJoin {
           ++ [(writeTextDir "lib/codedown/exporters.yaml" (lib.generators.toYAML {} exporterInfos))]
   ;
 
-  passthru = {
-    inherit ui_metadata ui_metadata_yaml;
+  passthru = rec {
+    ui_metadata = {
+      channels = lib.mapAttrsToList uiMetadata.mkChannelUiMetadata channels;
+
+      kernels = map uiMetadata.mkKernelUiMetadata builtKernels;
+
+      other_packages = map uiMetadata.mkOtherPackageUiMetadata otherPackages;
+    };
+
+    ui_metadata_yaml = writeText "ui-metadata.yaml" (lib.generators.toYAML {} ui_metadata);
   };
 }
---
+++ codedown/uiMetadata.nix
@@ -0,0 +1,42 @@
+{ lib }:
+
+rec {
+  chooseInterestingMeta = lib.filterAttrs (n: v:
+    n == "description"
+    || n == "homepage"
+    || n == "changelog"
+    || (n == "available" && !v)
+    || (n == "broken" && v)
+    || (n == "unfree" && v)
+    || (n == "unsupported" && v)
+    || (n == "insecure" && v)
+      # || (n == "license.spdxId") # TODO
+  );
+
+  mkChannelUiMetadata = name: channel: {
+    foo = "bar";
+  };
+
+  mkKernelUiMetadata = kernel: {
+    # Dry
+    channel = kernel.channel;
+    name = kernel.name;
+    packages = kernel.args.packages;
+    settings = if kernel ? "settings" then kernel.settings else {};
+
+    # Hydrated
+    display_name = if kernel.meta ? "displayName" then kernel.meta.displayName else null;
+    icon = if kernel.meta ? "icon" then kernel.meta.icon else null;
+    modes = kernel.modes;
+    settings_schema = if kernel ? "settingsSchema" then kernel.settingsSchema else {};
+
+    # TODO?
+    languageServerNames = [];
+  };
+
+  mkOtherPackageUiMetadata = package: {
+    channel = package.channel;
+    attr = package.attr;
+    meta = if package.contents ? "meta" then chooseInterestingMeta package.contents.meta else {};
+  };
+}
