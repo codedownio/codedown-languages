717823271319b4b199368411719567902d7909a9
thomasjm <tom@codedown.io>
2024-04-05 01:19:11 -0700

    Fix hashes again, tests almost all passing

--- languages/haskell/language-server-hls/hnls.nix
+++ languages/haskell/language-server-hls/hnls.nix
@@ -49,19 +49,19 @@ let
       });
       "v92" = prebuilt (fetchzip {
         url = "https://github.com/codedownio/haskell-notebook-language-server/releases/download/v0.3.0.0/haskell-notebook-language-server-0.3.0.0-ghc928-x86_64-linux.tar.gz";
-        sha256 = "1kwc25f7l5yxl55hnsjbxw8lhjw6hsd83212bafji95jach63ic5";
+        sha256 = "sha256-A3ZZqkrpypcn4UhhqCjtSLfNj9wemH58OSpiknrRl0Q=";
       });
       "v94" = prebuilt (fetchzip {
         url = "https://github.com/codedownio/haskell-notebook-language-server/releases/download/v0.3.0.0/haskell-notebook-language-server-0.3.0.0-ghc948-x86_64-linux.tar.gz";
-        sha256 = "17arbkja36zm18l73fn7khv65fyk13z6xb8zvc5kap74d2ryx23h";
+        sha256 = "sha256-Uj+V5HKA0QtRrc/jodMtmo+33TodX4LxuTwCRydAUME=";
       });
       "v96" = prebuilt (fetchzip {
         url = "https://github.com/codedownio/haskell-notebook-language-server/releases/download/v0.3.0.0/haskell-notebook-language-server-0.3.0.0-ghc964-x86_64-linux.tar.gz";
-        sha256 = "1d2bcrg6l1jsacmq3qv7rxkicgnm2l3i2ql8vfw2yj6gk0f5jx5d";
+        sha256 = "sha256-ITQNjAv03Mpaq99Hct1idkQzNITidcdtWbG1MSYbM7w=";
       });
       "v98" = prebuilt (fetchzip {
         url = "https://github.com/codedownio/haskell-notebook-language-server/releases/download/v0.3.0.0/haskell-notebook-language-server-0.3.0.0-ghc982-x86_64-linux.tar.gz";
-        sha256 = "0vfxapl3ssw6r173shd9xfj23gdyw81g0j2in2vak5dn45dvn44r";
+        sha256 = "sha256-jwJd8IQHIeAI5igX0eilu2e0mDyGn0qPhBf9qnTtbro=";
       });
     };
 
