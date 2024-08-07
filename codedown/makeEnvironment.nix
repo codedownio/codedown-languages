d616d1ec9ef5a59b3b17f3955647beacedec21bb
thomasjm <tom@codedown.io>
2024-08-07 06:53:25 -0700

    makeEnvironment looking good

--- codedown/makeEnvironment.nix
+++ codedown/makeEnvironment.nix
@@ -2,6 +2,7 @@
 , lib
 , fetchgit
 , fetchFromGitHub
+, system
 
 , mkCodeDownEnvironment
 , name ? "codedown-environment"
@@ -18,35 +19,41 @@ let
   importedOverlays = [];
 
   importedChannels = builtins.mapAttrs (name: value: let imported = import value; in
-    if (builtins.isFunction imported && builtins.hasAttr "overlays" (builtins.functionArgs imported)) then imported { overlays = importedOverlays; }
+    if (builtins.isFunction imported && builtins.hasAttr "overlays" (builtins.functionArgs imported)) then imported { overlays = importedOverlays; inherit system; }
     else if (builtins.isFunction imported) then imported { inherit fetchgit fetchFromGitHub; }
     else imported
   ) channels;
 
-  kernels = let
-    kernelPackages = filterAttrs (k: _: hasPrefix "codedown.kernels." k) packages;
-    in
-      mapAttrsToList (n: v: {
-        name = builtins.substring (builtins.stringLength "codedown.kernels.") (builtins.stringLength n) n;
-        channel = "codedown";
-        args = v;
-      }) kernelPackages;
+  kernelPackages = filterAttrs (k: _: hasPrefix "codedown.kernels." k) packages;
+  nonKernelPackages = filterAttrs (k: _: !(hasPrefix "codedown.kernels." k)) packages;
 
-  getByChannelAndPackageName = k: v: null;
-
-  otherPackages = let
-    byChannelAndPackageName = filterAttrs (n: v: v != null) (mapAttrs (k: v: getByChannelAndPackageName k v) packages);
+  getByChannelAndPackageName = k: v: let
+    parts = strings.splitString "." k;
     in
-      mapAttrsToList (n: v: {
-        channel = v.channel;
-        attr = v.attr;
-        contents = getAttr v.attr importedChannels.${v.channel};
-      }) byChannelAndPackageName;
+      if builtins.length parts == 1 then null
+      else {
+        channel = head parts;
+        attr = concatStringsSep "." (tail parts);
+      };
 
 in
 
 mkCodeDownEnvironment {
   channels = null;
   environmentName = name;
-  inherit kernels otherPackages;
+
+  kernels = mapAttrsToList (n: v: {
+    name = builtins.substring (builtins.stringLength "codedown.kernels.") (builtins.stringLength n) n;
+    channel = "codedown";
+    args = v;
+  }) kernelPackages;
+
+  otherPackages = let
+    byChannelAndPackageName = filterAttrs (n: v: v != null) (mapAttrs (k: v: getByChannelAndPackageName k v) nonKernelPackages);
+    in
+      mapAttrsToList (n: v: {
+        channel = v.channel;
+        attr = v.attr;
+        contents = getAttr v.attr importedChannels.${v.channel};
+      }) byChannelAndPackageName;
 }
