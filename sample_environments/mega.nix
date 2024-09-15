8924751b6a65b907c5ed0b0eb2276d99ea8712cc
thomasjm <tom@codedown.io>
2024-09-14 20:01:29 -0700

    Update mega environment to use modules

--- sample_environments/mega.nix
+++ sample_environments/mega.nix
@@ -5,253 +5,83 @@
 }:
 
 
-codedown.mkCodeDownEnvironment {
-  inherit channels;
+codedown.makeEnvironment channels {
+  kernels.bash.enable = true;
+  kernels.clojure.enable = true;
+  # kernels.cpp11.enable = true;
+  # kernels.cpp14.enable = true;
+  # kernels.cpp17.enable = true;
+  # kernels.cpp20.enable = true;
+  # kernels.cpp23.enable = true;
 
-  kernels = [
-    ({
-      name = "bash";
-      channel = "codedown";
-      args = {
-        packages = [];
-      };
-    })
+  # kernels.julia.enable = true;
+  # kernels.julia.packages = ["JSON3" "Plots"];
+  # kernels.julia.settings.lsp.LanguageServer.enable = true;
+  # kernels.julia.settings.lsp.LanguageServer.index = true;
+  # kernels.julia.settings.lsp.LanguageServer.debug = false;
 
-    # ({
-    #   name = "graphviz";
-    #   channel = "codedown";
-    #   args = {
-    #     packages = [];
-    #   };
-    # })
+  # kernels.julia16.enable = true;
+  # kernels.julia16.packages = ["JSON3" "Plots"];
 
-    ({
-      name = "clojure";
-      channel = "codedown";
-      args = {
-        packages = [];
-      };
-    })
+  # kernels.haskell-ghc92.enable = true;
+  # kernels.haskell-ghc92.packages = ["aeson"];
+  # kernels.haskell-ghc92.settings.lsp.haskell-language-server.debug = true;
 
-    ({
-      name = "cpp11";
-      channel = "codedown";
-      args = {
-        packages = [];
-        attrs = ["cpp11" "cpp"];
-      };
-    })
+  # kernels.haskell-ghc94.enable = true;
+  # kernels.haskell-ghc94.packages = ["aeson"];
+  # kernels.haskell-ghc94.settings.lsp.haskell-language-server.debug = true;
 
-    ({
-      name = "cpp14";
-      channel = "codedown";
-      args = {
-        packages = [];
-        attrs = ["cpp14" "cpp"];
-      };
-    })
+  # kernels.haskell-ghc96.enable = true;
+  # kernels.haskell-ghc96.packages = ["aeson"];
+  # kernels.haskell-ghc96.settings.lsp.haskell-language-server.debug = true;
 
-    ({
-      name = "cpp17";
-      channel = "codedown";
-      args = {
-        packages = [];
-        attrs = ["cpp17" "cpp"];
-      };
-    })
+  # kernels.haskell-ghc98.enable = true;
+  # kernels.haskell-ghc98.packages = ["aeson"];
+  # kernels.haskell-ghc98.settings.lsp.haskell-language-server.debug = true;
 
-    ({
-      name = "cpp20";
-      channel = "codedown";
-      args = {
-        packages = [];
-        attrs = ["cpp20" "cpp"];
-      };
-    })
+  kernels.octave.enable = true;
+  kernels.octave.packages = ["arduino"];
+  kernels.octave.settings.extraJupyterConfig = ''
+    c.OctaveKernel.plot_settings = dict(format='svg')
+  '';
 
-    ({
-      name = "cpp23";
-      channel = "codedown";
-      args = {
-        packages = [];
-        attrs = ["cpp23" "cpp"];
-      };
-    })
+  kernels.R.enable = true;
+  kernels.R.packages = ["ggplot2"];
 
-    ({
-      name = "julia";
-      channel = "codedown";
-      args = {
-        packages = ["JSON3" "Plots"];
-        settings = {
-          "lsp.LanguageServer.enable" = true;
-          "lsp.LanguageServer.index" = true;
-          "lsp.LanguageServer.debug" = false;
-        };
-      };
-    })
+  # kernels.python3.enable = true;
+  # kernels.python3.packages = ["matplotlib" "scipy" "rope"];
+  # kernels.python3.settings.permitUserSite = false;
+  # kernels.python3.settings.lsp.jedi.enable = true;
+  # kernels.python3.settings.lsp.pyright.enable = true;
+  # kernels.python3.settings.lsp.pylint.enable = true;
+  # kernels.python3.settings.lsp.flake8.enable = true;
+  # kernels.python3.settings.lsp.pycodestyle.enable = true;
+  # kernels.python3.settings.lsp.python-lsp-server.enable = true;
 
-    ({
-      name = "julia16";
-      channel = "codedown";
-      args = {
-        packages = ["JSON3" "Plots"];
-      };
-    })
+  # kernels.pypy3.enable = true;
+  # kernels.pypy3.settings.permitUserSite = false;
 
-    # ({
-    #   name = "haskell-ghc810";
-    #   channel = "codedown";
-    #   args = {
-    #     packages = ["aeson"];
-    #     settings = {
-    #       "lsp.haskell-language-server.debug" = true;
-    #     };
-    #   };
-    # })
+  kernels.ruby.enable = true;
 
-    ({
-      name = "haskell-ghc92";
-      channel = "codedown";
-      args = {
-        packages = ["aeson"];
-        settings = {
-          "lsp.haskell-language-server.debug" = true;
-        };
-      };
-    })
+  # kernels.rust.enable = true;
+  # kernels.rust.packages = ["rand"];
 
-    ({
-      name = "haskell-ghc94";
-      channel = "codedown";
-      args = {
-        packages = ["aeson"];
-        settings = {
-          "lsp.haskell-language-server.enable" = true;
-        };
-      };
-    })
+  kernels.go.enable = true;
 
-    ({
-      name = "haskell-ghc96";
-      channel = "codedown";
-      args = {
-        packages = ["aeson"];
-        settings = {
-          "lsp.haskell-language-server.enable" = true;
-        };
-      };
-    })
+  # kernels.postgres.enable = true;
 
-    ({
-      name = "haskell-ghc98";
-      channel = "codedown";
-      args = {
-        packages = ["aeson"];
-        settings = {
-          "lsp.haskell-language-server.debug" = true;
-        };
-      };
-    })
+  kernels.coq.enable = true;
 
-    ({
-      name = "octave";
-      channel = "codedown";
-      args = {
-        packages = ["arduino"];
-        extraJupyterConfig = ''
-          c.OctaveKernel.plot_settings = dict(format='svg')
-        '';
-      };
-    })
+  shells.bash.enable = true;
+  shells.fish.enable = true;
+  shells.zsh.enable = true;
 
-    ({
-      name = "R";
-      channel = "codedown";
-      args = {
-        packages = ["ggplot2"];
-      };
-    })
+  exporters.nbconvert-exporters.enable = true;
+  exporters.nbconvert-exporters.texliveScheme = "scheme-full";
 
-    # ({
-    #   name = "python3";
-    #   channel = "codedown";
-    #   args = {
-    #     packages = ["matplotlib" "scipy" "rope"];
-    #     settings = {
-    #       permitUserSite = false;
-    #       "lsp.jedi.enable" = true;
-    #       "lsp.pyright.enable" = true;
-    #       "lsp.pylint.enable" = true;
-    #       "lsp.flake8.enable" = true;
-    #       "lsp.pycodestyle.enable" = true;
-    #       "lsp.python-lsp-server.enable" = true;
-    #     };
-    #   };
-    # })
+  # otherPackages = [
+  #   { channel = "codedown"; attr = "spellchecker"; contents = codedown.spellchecker; }
 
-    # ({
-    #   name = "pypy3";
-    #   channel = "codedown";
-    #   args = {
-    #     packages = [];
-    #     settings = {
-    #       permitUserSite = false;
-    #     };
-    #   };
-    # })
-
-    ({
-      name = "ruby";
-      channel = "codedown";
-      args = {
-        packages = [];
-      };
-    })
-
-    ({
-      name = "rust";
-      channel = "codedown";
-      args = {
-        packages = ["rand"];
-      };
-    })
-
-    ({
-      name = "go";
-      channel = "codedown";
-      args = {
-        packages = [];
-      };
-    })
-
-    ({
-      name = "postgres";
-      channel = "codedown";
-      args = {
-        packages = [];
-      };
-    })
-
-    ({
-      name = "coq";
-      channel = "codedown";
-      args = {
-        packages = [];
-      };
-    })
-  ];
-
-  otherPackages = [
-    { channel = "codedown"; attr = "spellchecker"; contents = codedown.spellchecker; }
-
-    { channel = "codedown"; attr = "shells.zsh"; contents = codedown.shells.zsh; }
-    { channel = "codedown"; attr = "shells.fish"; contents = codedown.shells.fish; }
-    { channel = "codedown"; attr = "shells.bash"; contents = codedown.shells.bash; }
-
-    { channel = "codedown"; attr = "exporters.nbconvert-small"; contents = codedown.exporters.nbconvert-small; }
-    { channel = "codedown"; attr = "exporters.nbconvert-large"; contents = codedown.exporters.nbconvert-large; }
-
-    { channel = "nixpkgs"; attr = "htop"; contents = pkgsStable.htop; }
-  ];
+  #   { channel = "nixpkgs"; attr = "htop"; contents = pkgsStable.htop; }
+  # ];
 }
