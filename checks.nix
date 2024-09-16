ec48b5dc5e96682361b1d541fda4cbe47b636107
Tom McLaughlin <tom@codedown.io>
2024-09-16 15:32:01 -0700

    Get rid of old checks stuff

--- checks.nix
+++
@@ -1,7 +0,0 @@
-{
-  bash = import ./checks/bash.nix;
-  cpp = import ./checks/cpp.nix;
-  haskell = import ./checks/haskell.nix;
-  julia = import ./checks/julia.nix;
-  python = import ./checks/python.nix;
-}
