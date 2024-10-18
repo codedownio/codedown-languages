dd12c74f26f3ea50ccc16c19856598bbde132e2e
thomasjm <tom@codedown.io>
2024-10-18 05:44:22 -0700

    Try adding nixpkgs-overlay.nix

---
+++ nix/nixpkgs-overlay.nix
@@ -0,0 +1,34 @@
+{ symlinkJoin
+
+, chooseMeta
+
+, name ? "nixpkgs-environment"
+}:
+
+self: super: {
+  makeEnvironment = fakeModule:
+    symlinkJoin {
+      inherit name;
+      paths = let
+        getOutputs = pkg:
+          let
+            outputs = (fakeModule.${pkg}.settings.outputs or null);
+          in
+            if outputs != null
+            then (map (x: self.${pkg}.${x}) outputs)
+            else [self.${pkg}];
+      in
+        self.lib.concatMap getOutputs (builtins.attrNames fakeModule);
+
+      passthru = {
+        ui_metadata = {
+          packages = self.lib.mapAttrs (n: v: {
+            name = n;
+            meta = chooseMeta (self.${n} or {});
+            packages = [];
+            settings = v.settings or {};
+          }) fakeModule;
+        };
+      };
+    };
+}
