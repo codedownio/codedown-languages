3acb39887c4df8da4e1c106a9ad825d40890139e
thomasjm <tom@codedown.io>
2024-12-04 18:16:55 -0800

    Add GHC.Stack import

--- tests/src/TestLib/TestSearchers.hs
+++ tests/src/TestLib/TestSearchers.hs
@@ -14,6 +14,7 @@ import qualified Data.List as L
 import Data.String.Interpolate
 import Data.Text as T
 import qualified Data.Text.IO as T
+import GHC.Stack
 import System.FilePath
 import Test.Sandwich
 import TestLib.TestBuilding
