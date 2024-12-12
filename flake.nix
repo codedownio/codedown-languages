d43a01b86352aecf0bd2cbcfe74eba7bf8f5fef7
thomasjm <tom@codedown.io>
2024-12-12 02:30:05 -0800

    wip

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "CodeDown languages";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
   inputs.nixpkgs-master.url = "github:NixOS/nixpkgs/master";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
