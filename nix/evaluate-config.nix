05c23da4e951a6006bfb68e62c647dc63ab64d0e
Tom McLaughlin <tom@codedown.io>
2025-03-07 02:50:40 -0800

    Trim out modules to test caching

--- nix/evaluate-config.nix
+++ nix/evaluate-config.nix
@@ -21,27 +21,27 @@ lib.evalModules {
 
     ../modules/environment/module.nix
 
-    ../modules/exporters/module.nix
+    # ../modules/exporters/module.nix
 
-    ../modules/kernels/bash/module.nix
-    ../modules/kernels/clojure/module.nix
-    ../modules/kernels/coq/module.nix
-    ../modules/kernels/cpp/module.nix
-    ../modules/kernels/go/module.nix
-    ../modules/kernels/haskell/module.nix
-    ../modules/kernels/julia/module.nix
-    ../modules/kernels/octave/module.nix
-    ../modules/kernels/postgres/module.nix
-    ../modules/kernels/python/module.nix
-    ../modules/kernels/r/module.nix
-    ../modules/kernels/ruby/module.nix
-    ../modules/kernels/rust/module.nix
+    # ../modules/kernels/bash/module.nix
+    # ../modules/kernels/clojure/module.nix
+    # ../modules/kernels/coq/module.nix
+    # ../modules/kernels/cpp/module.nix
+    # ../modules/kernels/go/module.nix
+    # ../modules/kernels/haskell/module.nix
+    # ../modules/kernels/julia/module.nix
+    # ../modules/kernels/octave/module.nix
+    # ../modules/kernels/postgres/module.nix
+    # ../modules/kernels/python/module.nix
+    # ../modules/kernels/r/module.nix
+    # ../modules/kernels/ruby/module.nix
+    # ../modules/kernels/rust/module.nix
 
-    ../modules/language_servers/markdown-spellcheck-lsp/module.nix
+    # ../modules/language_servers/markdown-spellcheck-lsp/module.nix
 
-    ../modules/shells/bash/module.nix
-    ../modules/shells/fish/module.nix
-    ../modules/shells/zsh/module.nix
+    # ../modules/shells/bash/module.nix
+    # ../modules/shells/fish/module.nix
+    # ../modules/shells/zsh/module.nix
 
     {
       config = {
