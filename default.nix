f7232947c03826396952905e6d35c15fe1ce2a0f
Tom McLaughlin <tom@codedown.io>
2025-03-29 16:08:39 -0700

    Switching fetchTarball to fetchTree noci

--- default.nix
+++ default.nix
@@ -4,14 +4,20 @@
 }:
 
 let
-  pkgsStable = import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/6af28b834daca767a7ef99f8a7defa957d0ade6f.tar.gz"; # nixpkgs-rev
-    sha256 = "0afmlbvgky283wd2qjn7l19k1zzh454x6z97cdc22rnnzgfik1jv"; # nixpkgs-sha256
+  pkgsStable = import (builtins.fetchTree {
+    type = "github";
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "6af28b834daca767a7ef99f8a7defa957d0ade6f"; # nixpkgs-rev
+    narHash = "sha256-W4YZ3fvWZiFYYyd900kh8P8wU6DHSiwaH0j4+fai1Sk="; # nixpkgs-sha256
   }) { inherit overlays; };
 
   pkgsMaster = import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/25068c534b2c34bbff27c71af515177ad0cce061.tar.gz"; # nixpkgs-master-rev
-    sha256 = "0n3lzn8na97k2gfpfy975g6830p1gxi53yas885w0v5kqb75mv2h"; # nixpkgs-master-sha256
+    type = "github";
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "25068c534b2c34bbff27c71af515177ad0cce061"; # nixpkgs-master-rev
+    narHash = "sha256-UOxazsKzbMALQlr5UWJ/4YKBzCsneXfdE/MkZZH9dFg="; # nixpkgs-master-sha256
   }) { inherit overlays; };
 
 in
