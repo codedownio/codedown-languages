03dfb0cd1b2a1920fdc53164fdd9052f3d59a090
Tom McLaughlin <tom@codedown.io>
2025-04-02 03:54:56 -0700

    Use builtins.fetchTarball and fix update-default-nix

--- default.nix
+++ default.nix
@@ -12,14 +12,11 @@ let
     rev = "6af28b834daca767a7ef99f8a7defa957d0ade6f"; # nixpkgs-rev
     hash = "sha256-W4YZ3fvWZiFYYyd900kh8P8wU6DHSiwaH0j4+fai1Sk="; # nixpkgs-hash
   };
-  stableFetchTree = builtins.fetchTree {
-    type = "github";
-    owner = "NixOS";
-    repo = "nixpkgs";
-    rev = "6af28b834daca767a7ef99f8a7defa957d0ade6f"; # nixpkgs-rev
-    narHash = "sha256-W4YZ3fvWZiFYYyd900kh8P8wU6DHSiwaH0j4+fai1Sk="; # nixpkgs-hash
+  stableBuiltins = builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/6af28b834daca767a7ef99f8a7defa957d0ade6f.tar.gz"; # nixpkgs-rev
+    sha256 = "0afmlbvgky283wd2qjn7l19k1zzh454x6z97cdc22rnnzgfik1jv"; # nixpkgs-sha256
   };
-  pkgsStable = import (if fetchFromGitHub != null then stableFetchFromGitHub else stableFetchTree) ({
+  pkgsStable = import (if fetchFromGitHub != null then stableFetchFromGitHub else stableBuiltins) ({
     inherit overlays;
   } // (if system == null then {} else { inherit system; }));
 
@@ -29,14 +26,11 @@ let
     rev = "25068c534b2c34bbff27c71af515177ad0cce061"; # nixpkgs-master-rev
     hash = "sha256-UOxazsKzbMALQlr5UWJ/4YKBzCsneXfdE/MkZZH9dFg="; # nixpkgs-master-hash
   };
-  masterFetchTree = builtins.fetchTree {
-    type = "github";
-    owner = "NixOS";
-    repo = "nixpkgs";
-    rev = "25068c534b2c34bbff27c71af515177ad0cce061"; # nixpkgs-master-rev
-    narHash = "sha256-UOxazsKzbMALQlr5UWJ/4YKBzCsneXfdE/MkZZH9dFg="; # nixpkgs-master-hash
+  masterBuiltins = builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/25068c534b2c34bbff27c71af515177ad0cce061.tar.gz"; # nixpkgs-master-rev
+    sha256 = "0n3lzn8na97k2gfpfy975g6830p1gxi53yas885w0v5kqb75mv2h"; # nixpkgs-master-sha256
   };
-  pkgsMaster = import (if fetchFromGitHub != null then masterFetchFromGitHub else masterFetchTree) ({
+  pkgsMaster = import (if fetchFromGitHub != null then masterFetchFromGitHub else masterBuiltins) ({
     inherit overlays;
   } // (if system == null then {} else { inherit system; }));
 
