029a4c2704c7ca6ab05a197bb6966f85343fbb7e
Tom McLaughlin <tom@codedown.io>
2025-09-27 18:34:11 -0700

    Bump nixpkgsMaster to fix xeus-cling on darwin

--- default.nix
+++ default.nix
@@ -22,16 +22,16 @@ let
     inherit overlays;
   } // (if system == null then {} else { inherit system; }));
 
-  masterRev = "80bda06affb1d66698d7bf12806920942c8fd518"; # nixpkgs-master-rev
+  masterRev = "06aacd50ad037906523bff549f2ba50f2be7b697"; # nixpkgs-master-rev
   masterFetchFromGitHub = fetchFromGitHub {
     owner = "NixOS";
     repo = "nixpkgs";
     rev = masterRev;
-    hash = "sha256-J4YeCDLZSrD1+AgDh0dDOg23ldpSJfNze4TXjZ7yYro="; # nixpkgs-master-hash
+    hash = "sha256-IvwsyfvavAUL3a07xxMfHZ0X437OhjUWLq/eGcv7WeM="; # nixpkgs-master-hash
   };
   masterBuiltins = builtins.fetchTarball {
     url = ''https://github.com/NixOS/nixpkgs/archive/${masterRev}.tar.gz'';
-    sha256 = "1fk2yag8vmw4gdrz69ajvaavf39s8d3qf0q8z3sv0jnr6841x1i7"; # nixpkgs-master-sha256
+    sha256 = "1qsrzg5ikpmg5qb3b1nfgviig78x3w9wffxdvl5hbg6szg4jrz12"; # nixpkgs-master-sha256
   };
   pkgsMasterSrc = if fetchFromGitHub != null then masterFetchFromGitHub else masterBuiltins;
   pkgsMaster = import pkgsMasterSrc ({
