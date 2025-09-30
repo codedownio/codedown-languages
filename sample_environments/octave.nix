9f756818aecb3f8b95967a5ba32f2651172fe91f
Tom McLaughlin <tom@codedown.io>
2025-09-29 17:55:47 -0700

    Comment octave.withPackages usage since it's broken on darwin for now

--- sample_environments/octave.nix
+++ sample_environments/octave.nix
@@ -9,4 +9,6 @@ codedown.makeEnvironment {
   kernels.octave.extraJupyterConfig = ''
     c.OctaveKernel.plot_settings = dict(format='svg')
   '';
+
+  # kernels.octave.packages = ["doctest"]; # TODO: uncomment after https://github.com/NixOS/nixpkgs/pull/447122
 }
