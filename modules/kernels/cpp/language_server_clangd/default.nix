2bda7440fdbccf033caeb2f5a715bb6c86aade38
Tom McLaughlin <tom@codedown.io>
2025-11-12 19:26:47 -0800

    Clangd (#82)
    * Working on clangd language server noci
    
    * More on Cpp.hs test noci

---
+++ modules/kernels/cpp/language_server_clangd/default.nix
@@ -0,0 +1,32 @@
+{ lib
+, callPackage
+, llvmPackages
+
+, kernelName
+}:
+
+let
+  common = callPackage ../../common.nix {};
+
+  clangd = llvmPackages.clang-tools;
+
+  languageServerName = "clangd";
+
+  passthru = {
+    inherit languageServerName;
+  };
+
+in
+
+common.writeTextDirWithMetaAndPassthru clangd.meta passthru "lib/codedown/language-servers/cpp-${kernelName}-clangd.yaml" (lib.generators.toYAML {} [{
+  name = languageServerName;
+  version = clangd.version;
+  extensions = ["cpp" "hpp" "cxx" "hxx" "c" "h"];
+  notebook_suffix = ".cpp";
+  attrs = ["cpp"];
+  type = "stream";
+  primary = true;
+  args = [
+    "${clangd}/bin/clangd"
+  ];
+}])
--- tests/app/Spec/Tests/Cpp.hs
+++ tests/app/Spec/Tests/Cpp.hs
@@ -1,12 +1,15 @@
 {-# LANGUAGE RankNTypes #-}
 {-# OPTIONS_GHC -fno-warn-unused-top-binds #-}
+{-# OPTIONS_GHC -fno-warn-incomplete-uni-patterns #-}
 
 module Spec.Tests.Cpp (tests) where
 
 import Data.String.Interpolate
 import Data.Text
+import Language.LSP.Protocol.Types
 import Test.Sandwich as Sandwich
 import TestLib.JupyterRunnerContext
+import TestLib.LSP
 import TestLib.NixEnvironmentContext
 import TestLib.NixTypes
 import TestLib.TestSearchers
@@ -26,24 +29,47 @@ tests = describe "C++" $ parallel $ do
   tests' "c++23"
   tests' "c++2c"
 
+  testsWithLsp "c++23"
+
 tests' :: Text -> LanguageSpec
 tests' flavor = describe [i|C++ (#{flavor})|] $ introduceNixEnvironment [kernelSpec flavor] [] "C++" $ introduceJupyterRunner $ do
   testKernelStdout "cpp" [__i|\#include <iostream>
                               using namespace std;
                               cout << "hi" << endl;|] "hi\n"
 
+testsWithLsp :: Text -> LanguageSpec
+testsWithLsp flavor = describe [i|C++ (#{flavor}) with LSP|] $ introduceNixEnvironment [kernelSpecWithLsp flavor] [] "C++" $ do
+  describe "LSP" $ do
+    testDiagnostics'' "simple" lsName "test.cpp" (Just "cpp")
+      [__i|int main() {
+             undefined_function();
+             return 0;
+           }|] [] $ \diags -> do
+          info [i|Got diags: #{diags}|]
+          info [i|Got ranges: #{getDiagnosticRanges' diags}|]
+          getDiagnosticRanges' diags `shouldBe` [(Range (Position 1 2) (Position 1 20), Just (InR "undeclared_var_use"), "Use of undeclared identifier 'undefined_function'")]
+
+lsName :: Text
+lsName = "clangd"
 
 kernelSpec :: Text -> NixKernelSpec
-kernelSpec flavor  = NixKernelSpec {
+kernelSpec flavor  = kernelSpec' [[i|flavor = "#{flavor}"|]]
+
+kernelSpecWithLsp :: Text -> NixKernelSpec
+kernelSpecWithLsp flavor = kernelSpec' [
+  [i|flavor = "#{flavor}"|]
+    , "lsp.clangd.enable = true"
+    ]
+
+kernelSpec' :: [Text] -> NixKernelSpec
+kernelSpec' extraConfig = NixKernelSpec {
   nixKernelName = "cpp"
   , nixKernelChannel = "codedown"
   , nixKernelDisplayName = Just "CPP"
   , nixKernelPackages = []
   , nixKernelMeta = Nothing
   , nixKernelIcon = Nothing
-  , nixKernelExtraConfig = Just [
-      [i|flavor = "#{flavor}"|]
-      ]
+  , nixKernelExtraConfig = Just extraConfig
   }
 
 main :: IO ()
