8c70638acf265665ea7ff60c128543bf041ee37e
Tom McLaughlin <tom@codedown.io>
2026-01-19 16:39:03 -0800

    cling-parser: pass the proper flags

--- modules/kernels/cpp/language_server_clangd/cling-parser.nix
+++ modules/kernels/cpp/language_server_clangd/cling-parser.nix
@@ -1,37 +1,70 @@
-{ lib
-, stdenv
-, fetchFromGitHub
+{ cling
 , cmake
-, cling
-, zlib
+, fetchFromGitHub
+, lib
 , ncurses
+, stdenv
+, makeWrapper
+, zlib
 }:
 
-stdenv.mkDerivation rec {
-  pname = "cling-parser";
-  version = import ./cnls-version.nix;
+let
+  unwrapped = stdenv.mkDerivation rec {
+    pname = "cling-parser";
+    version = import ./cnls-version.nix;
 
-  src = fetchFromGitHub {
-    owner = "codedownio";
-    repo = "cpp-notebook-language-server";
-    rev = "v${version}";
-    hash = "sha256-6dKv2PMqam8R196IAEjh2uRROQLGOYKw4n/gztmmr6A=";
+    src = fetchFromGitHub {
+      owner = "codedownio";
+      repo = "cpp-notebook-language-server";
+      rev = "v${version}";
+      hash = "sha256-6dKv2PMqam8R196IAEjh2uRROQLGOYKw4n/gztmmr6A=";
+    };
+
+    sourceRoot = "${src.name}/cling-parser";
+
+    nativeBuildInputs = [ cmake ];
+
+    buildInputs = [ cling.unwrapped zlib ncurses ];
+
+    cmakeFlags = [
+      "-DLLVM_CONFIG=${cling.unwrapped}/bin/llvm-config"
+    ];
+
+    meta = with lib; {
+      description = "Minimal Cling parser for cpp-notebook-language-server";
+      homepage = "https://github.com/codedownio/cpp-notebook-language-server";
+      license = licenses.bsd3;
+      platforms = platforms.all;
+    };
   };
 
-  sourceRoot = "${src.name}/cling-parser";
+in
 
-  nativeBuildInputs = [ cmake ];
+unwrapped.overrideAttrs (oldAttrs: {
+  nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ makeWrapper ];
 
-  buildInputs = [ cling.unwrapped zlib ncurses ];
-
-  cmakeFlags = [
-    "-DLLVM_CONFIG=${cling.unwrapped}/bin/llvm-config"
+  # cling-parser needs a collection of flags to start up properly, so wrap it by default.
+  # We'll provide the unwrapped version as a passthru
+  flags = cling.flags ++ [
+    "-resource-dir"
+    "${cling.unwrapped}"
+    "-L"
+    "${cling.unwrapped}/lib"
+    "-l"
+    "${cling.unwrapped}/lib/cling.so"
   ];
 
-  meta = with lib; {
-    description = "Minimal Cling parser for cpp-notebook-language-server";
-    homepage = "https://github.com/codedownio/cpp-notebook-language-server";
-    license = licenses.bsd3;
-    platforms = platforms.all;
+  fixupPhase = ''
+    runHook preFixup
+
+    wrapProgram $out/bin/cling-parser \
+      --argv0 $out/bin/.cling-parser-wrapped \
+      --add-flags "$flags"
+
+    runHook postFixup
+  '';
+
+  passthru = (oldAttrs.passthru or { }) // {
+    inherit unwrapped;
   };
-}
+})
