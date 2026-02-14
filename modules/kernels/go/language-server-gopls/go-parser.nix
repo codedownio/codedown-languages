b36731eccd48e09515d7cf146f7ced633c6a5b9a
Tom McLaughlin <tom@codedown.io>
2026-02-13 19:04:50 -0800

    Update go-parser.nix hash

--- modules/kernels/go/language-server-gopls/go-parser.nix
+++ modules/kernels/go/language-server-gopls/go-parser.nix
@@ -11,7 +11,7 @@ buildGoModule rec {
     owner = "codedownio";
     repo = "go-notebook-language-server";
     rev = "v${version}";
-    hash = "sha256-SdN/v4psQRQ8O+3BosYgcZzBZSxNytVmw1A/lM2fQ/4=";
+    hash = "sha256-bOh7LbPxs+olEGKrNnnoq6xMhoRRSeZCV6GgJ7CUPyk=";
   };
 
   sourceRoot = "${src.name}/go-parser";
