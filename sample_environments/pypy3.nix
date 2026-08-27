0d4f902f3e929081c7f0dde4cd31d9e5637c2183
thomasjm <tom@codedown.io>
2026-08-27 04:04:34 -0700

    Cover the pypy3 kernel with a sample environment and a kernel test

--- sample_environments/pypy3.nix
+++ sample_environments/pypy3.nix
@@ -5,10 +5,5 @@
 codedown.makeEnvironment {
   name = "pypy3";
 
-  # kernels.pypy3.enable = true;
-  # kernels.pypy3.packages = [
-  #   "matplotlib"
-  #   "scipy"
-  #   "rope"
-  # ];
+  kernels.pypy3.enable = true;
 }
