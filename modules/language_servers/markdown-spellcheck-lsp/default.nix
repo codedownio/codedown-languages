b2df1de509cd0f586904de0b5487a3a809803160
thomasjm <tom@codedown.io>
2026-09-24 16:23:36 -0700

    Bump markdown-spellcheck-lsp to 0.6.5

--- modules/language_servers/markdown-spellcheck-lsp/default.nix
+++ modules/language_servers/markdown-spellcheck-lsp/default.nix
@@ -32,14 +32,14 @@ let
   #   tar -czvf $out "$dir"
   # '';
 
-  version = "0.6.3";
+  version = "0.6.5";
 
   indexJs = stdenv.mkDerivation {
     name = "markdown-spellcheck-lsp-index.js";
 
     src = fetchurl {
       url = "https://github.com/codedownio/markdown-spellcheck-lsp/releases/download/v${version}/markdown-spellcheck-lsp.tar.gz";
-      hash = "sha256-99bGac3f15WO4AEG/n9UeoSTaHMjXf9xnKxuT434JnU=";
+      hash = "sha256-58NwVV+Qdz6efbblUUxVHXNI/lwMwhjHXMbqxZ+iJjY=";
     };
 
     buildPhase = "true";
